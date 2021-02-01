55269e92414f62c727324ead48f26917709ab7b2
Bonvallat <dan.bnvll@eduge.ch>
2021-02-01 11:31:50 +0100

    chapitre 2

---
+++ Chap2/php/constantes.php
@@ -0,0 +1,6 @@
+<?php
+
+define("HOST", "localhost");
+define("DBNAME", "db_m152");
+define("DBUSER", "Bob");
+define("DBPWD", "Bonval22");
--- Chap2/post.php
+++ Chap2/post.php
@@ -1,25 +1,41 @@
 <?php
-$img = filter_input(INPUT_POST,'lienImg', FILTER_SANITIZE_STRING);
+require_once "php/function.php";
+
+function affichePost()
+{
+    $msg = filter_input(INPUT_POST, 'commentaire', FILTER_SANITIZE_STRING);
+    addPost($msg);
+    $id = retournId();
+    var_dump($id);
+}
+
+if (isset($_POST['envoie']))
+{
+    affichePost();
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Post</title>
 </head>
+
 <body>
     <p>
         <a href="index.php">Page d'acueille</a>
     </p>
     <form method="POST" enctype="multipart/form-data" action="#">
-        <textarea placeholder="Ajouter un commentaire" style="resize:none;" rows="5" cols="55"></textarea><br>
+        <textarea placeholder="Ajouter un commentaire" style="resize:none;" rows="5" cols="55" name="commentaire"></textarea><br>
         <label>Choisir une image</label>
         <input type="file" multiple id="takeImage" accept="image/*" name="lienImg[]"><br>
         <input type="submit" value="Envoyer" name="envoie">
     </form>
 
 </body>
+
 </html>
